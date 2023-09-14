27f6acf10a7f4645d7d824332b4d941fa0ab1ca2
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-14 11:52:45 +0700

    Animate the size of a composable

--- app/src/main/java/com/rujirakongsomran/jc_animate/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_animate/MainActivity.kt
@@ -3,13 +3,25 @@ package com.rujirakongsomran.jc_animate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_animate.ui.theme.JC_AnimateTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +34,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AnimateSizeComposable() {
+    var expended by remember {
+        mutableStateOf(false)
+    }
+    Box(
+        modifier = Modifier
+            .background(Color.Blue)
+            .animateContentSize()
+            .height(if (expended) 400.dp else 200.dp)
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = remember {
+                    MutableInteractionSource()
+                },
+                indication = null
+            ) {
+                expended = !expended
+            }
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JC_AnimateTheme {
-        Greeting("Android")
     }
 }
+
